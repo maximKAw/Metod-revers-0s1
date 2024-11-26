cf76a015bc06a002afd9a498021d871f7b5a2a86
maximKAw <168087872+maximKAw@users.noreply.github.com>
2024-11-26 17:08:37 +0900

    Initial commit

---
+++ Metod revers 0s1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+public class ArraySESS
+{
+    // Метод сортировки массива по возрастанию
+    public static int[] SortAscending(int[] arr)
+    {
+        return arr.OrderBy(x => x).ToArray();
+    }
+
+    // Метод сортировки массива по убыванию
+    public static int[] SortDescending(int[] arr)
+    {
+        return arr.OrderByDescending(x => x).ToArray();
+    }
+
+    // Метод объединения двух массивов в один
+    public static int[] ConcatenateArrays(int[] arr1, int[] arr2)
+    {
+        return arr1.Concat(arr2).ToArray();
+    }
+}
